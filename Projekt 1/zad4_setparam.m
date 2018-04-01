def529b0142ea466ba936926f86031ff5155f888
kuba <J.Sikora@stud.elka.pw.edu.pl>
2018-04-01 23:48:40 +0200

    Stworzone środowisko do symulacji zadania 4

---
+++ Projekt 1/zad4_setparam.m
@@ -0,0 +1,11 @@
+% STP - Projekt 1 - Zadanie 47 - Jakub Sikora
+% Ustawienie parametrów symulacji procesu
+clear;
+
+b0 = 0.0464;
+b1 = -0.3684;
+b2 = 0.4496;
+
+a0 = 0.0636;
+a1 = -1.3904;
+a2 = 7.576;
